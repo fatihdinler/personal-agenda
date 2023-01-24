b5ce053fa0c7bc678386e4fbe57816c8008e61dd
fatiherkamdinler <fatihdinler458@gmail.com>
2023-01-24 20:00:49 +0300

    Reinitial commit

--- personal-agenda/src/colors/colors.js
+++
@@ -1,6 +0,0 @@
-export default {
-    BLUE : '#2F80DB',
-    LIGTH_BLUE : '#69A3E4',
-    LIGHT_ACCENT_BLUE : '#0CC3F0',
-    WHITE : '#FFFFFF',
-}
--- personal-agenda/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import taskSlice from "./task-slice"
-
-export const store = configureStore({
-    reducer : {
-        tasks : taskSlice
-    },
-})
